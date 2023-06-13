537da7b51f8ecfd5dd47f11842a569ea36e078de
Pedro Mariano <ph_marianocorrea@hotmail.com>
2023-06-13 16:01:43 -0300

    Update Explosion class to have a radius and to initialize a timer on init to trigger it

--- app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
+++ app/src/main/kotlin/galaxyraiders/core/game/Explosion.kt
@@ -1,17 +1,30 @@
 package galaxyraiders.core.game
 
+import java.util.Timer
+import java.util.TimerTask
 import galaxyraiders.core.physics.Point2D
 import galaxyraiders.core.physics.Vector2D
 
 class Explosion(
-  position: Point2D
+  position: Point2D,
+  radius: Double
 ) :
-  SpaceObject("Explosion", '*', position, Vector2D(0.0, 0.0), 4.0, 0.0) {
+  SpaceObject("Explosion", '*', position, Vector2D(0.0, 0.0), radius, 0.0) {
 
-  var is_triggered: Boolean = false
+  var isTriggered: Boolean = true
     private set
 
   fun triggered() {
-    this.is_triggered = true
+    this.isTriggered = false
+  }
+
+  init {
+    var timerTask = object : TimerTask() {
+      override fun run() {
+        triggered()
+      }
+    }
+
+    Timer().schedule(timerTask, 1000)
   }
 }
