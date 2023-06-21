572d5e315e5a3d806944a72990e0003bf554094f
Pedro Mariano <ph_marianocorrea@hotmail.com>
2023-06-21 15:15:57 -0300

    Refactor Explosion class test

--- app/src/test/kotlin/galaxyraiders/core/game/ExplosionTest.kt
+++ app/src/test/kotlin/galaxyraiders/core/game/ExplosionTest.kt
@@ -1,7 +1,6 @@
 package galaxyraiders.core.game
 
 import galaxyraiders.core.physics.Point2D
-import galaxyraiders.core.physics.Vector2D
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
@@ -12,27 +11,34 @@ import kotlin.test.assertTrue
 
 @DisplayName("Given an explosion")
 class ExplosionTest {
-  private val e = Explosion(
-    position = Point2D(1.0, 1.0)
+  private val explosion = Explosion(
+    position = Point2D(1.0, 1.0),
+    radius = 2.0
   )
 
   @Test
   fun `it has its parameters initialized correctly `() {
     assertAll(
       "Explosion should initialize all its parameters correctly",
-      { assertNotNull(e) },
-      { assertEquals(Point2D(1.0, 1.0), e.position) },
+      { assertNotNull(explosion) },
+      { assertEquals(Point2D(1.0, 1.0), explosion.center) },
+      { assertEquals(2.0, explosion.radius) },
+      { assertEquals(true, explosion.isTriggered) }
     )
   }
 
   @Test
-  fun `it is not triggered by default `() {
-    assertFalse(e.is_triggered)
+  fun `it has a type Explosion `() {
+    assertEquals("Explosion", explosion.type)
   }
 
   @Test
-  fun `it can be triggered `() {
-    e.triggered()
-    assertTrue(e.is_triggered)
+  fun `it has a symbol asterisk `() {
+    assertEquals('*', explosion.symbol)
+  }
+
+  @Test
+  fun `it shows the type Explosion when converted to String `() {
+    assertTrue(explosion.toString().contains("Explosion"))
   }
 }
